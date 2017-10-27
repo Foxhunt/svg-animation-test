79d440e88c53517341dcc2e743f34da3fc60b464
Marco <marco.mojica@artcom.de>
2017-10-27 17:12:26 +0200

    Add second animation group

--- svg-main.js
+++ svg-main.js
@@ -7,6 +7,9 @@ var lines = draw.group()
 
 draw.circle().radius(4).center(90, 90)
 
+var lineAnims = []
+var lineAnims2 = []
+
 
 circles.circle().radius(4).center(90, 90)
   .animate(duration).center(40, 40)
@@ -17,10 +20,16 @@ circles.circle().radius(4).center(90, 90)
 circles.circle().radius(4).center(90, 90)
   .animate(duration).center(40, 140)
 
-lines.line(40, 40, 140, 40)
-lines.line(140, 40, 140, 140)
-lines.line(140, 140, 40, 140)
-lines.line(40, 140, 40, 40)
+lineAnims.push(lines.line(40, 40, 40, 40).animate(1000).plot(40, 40, 140, 40).pause())
+lineAnims.push(lines.line(140, 40, 140, 40).animate(1000).plot(140, 40, 140, 140).pause())
+lineAnims.push(lines.line(140, 140, 140, 140).animate(1000).plot(140, 140, 40, 140).pause())
+lineAnims.push(lines.line(40, 140, 40, 140).animate(1000).plot(40, 140, 40, 40).pause())
+
+lineAnims2.push(lines.line(40, 40, 40, 40).animate(300).plot(40, 40, 90, 90).pause())
+lineAnims2.push(lines.line(140, 40, 140, 40).animate(300).plot(140, 40, 90, 90).pause())
+lineAnims2.push(lines.line(140, 140, 140, 140).animate(300).plot(140, 140, 90, 90).pause())
+lineAnims2.push(lines.line(40, 140, 40, 140).animate(300).plot(40, 140, 90, 90).pause())
+
 lines.stroke({
   width: 2,
   color: '#000'
@@ -28,4 +37,4 @@ lines.stroke({
 
 var all = draw.group().add(lines).add(circles)
 
-all.animate(3000).rotate(45)
+all.animate(duration).rotate(45).after(() => {lineAnims.forEach(a => a.play().after(() => {lineAnims2.forEach(a => a.play())}))})
