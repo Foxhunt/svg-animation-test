f02607c011d3188d81d7dbe2c1d3a4180feaabb3
Marco <marco.mojica@hotmail.de>
2017-10-30 19:06:48 +0100

    Change animation behavior

--- svg-main.js
+++ svg-main.js
@@ -1,6 +1,6 @@
 var draw = SVG('drawing').size(300, 300)
 
-const duration = 1000 * 4;
+const duration = 1000 * 2;
 
 var circles = draw.group()
 var lines = draw.group()
@@ -8,33 +8,72 @@ var lines = draw.group()
 draw.circle().radius(4).center(90, 90)
 
 var lineAnims = []
-var lineAnims2 = []
+var circleAnims = []
+
+var rotation = 45;
 
 
-circles.circle().radius(4).center(90, 90)
-  .animate(duration).center(40, 40)
-circles.circle().radius(4).center(90, 90)
-  .animate(duration).center(140, 40)
-circles.circle().radius(4).center(90, 90)
-  .animate(duration).center(140, 140)
-circles.circle().radius(4).center(90, 90)
-  .animate(duration).center(40, 140)
+circleAnims.push(
+	circles.circle().radius(4).center(90, 90)
+	.animate(duration).center(40, 40)
+	.pause()
+)
+circleAnims.push(
+	circles.circle().radius(4).center(90, 90)
+	.animate(duration).center(140, 40)
+	.pause()
+)
+circleAnims.push(
+	circles.circle().radius(4).center(90, 90)
+	.animate(duration).center(140, 140)
+	.pause()
+)
+circleAnims.push(
+	circles.circle().radius(4).center(90, 90)
+	.animate(duration).center(40, 140)
+	.pause()
+)
 
-lineAnims.push(lines.line(40, 40, 40, 40).animate(1000).plot(40, 40, 140, 40).pause())
-lineAnims.push(lines.line(140, 40, 140, 40).animate(1000).plot(140, 40, 140, 140).pause())
-lineAnims.push(lines.line(140, 140, 140, 140).animate(1000).plot(140, 140, 40, 140).pause())
-lineAnims.push(lines.line(40, 140, 40, 140).animate(1000).plot(40, 140, 40, 40).pause())
-
-lineAnims2.push(lines.line(40, 40, 40, 40).animate(300).plot(40, 40, 90, 90).pause())
-lineAnims2.push(lines.line(140, 40, 140, 40).animate(300).plot(140, 40, 90, 90).pause())
-lineAnims2.push(lines.line(140, 140, 140, 140).animate(300).plot(140, 140, 90, 90).pause())
-lineAnims2.push(lines.line(40, 140, 40, 140).animate(300).plot(40, 140, 90, 90).pause())
+lineAnims.push(
+	lines.line(40, 40, 40, 40)
+	.animate(duration / 3).plot(40, 40, 140, 40)
+	.animate(duration / 3).plot(140, 40, 140, 40)
+	.animate(duration / 3).plot(140, 140, 140, 40)
+	.pause()
+)
+lineAnims.push(
+	lines.line(140, 40, 140, 40)
+	.animate(duration / 3).plot(140, 40, 140, 140)
+	.animate(duration / 3).plot(140, 140, 140, 140)
+	.animate(duration / 3).plot(40, 140, 140, 140)
+	.pause()
+)
+lineAnims.push(
+	lines.line(140, 140, 140, 140)
+	.animate(duration / 3).plot(140, 140, 40, 140)
+	.animate(duration / 3).plot(40, 140, 40, 140)
+	.animate(duration / 3).plot(40, 40, 40, 140)
+	.pause()
+)
+lineAnims.push(
+	lines.line(40, 140, 40, 140)
+	.animate(duration / 3).plot(40, 140, 40, 40)
+	.animate(duration / 3).plot(40, 40, 40, 40)
+	.animate(duration / 3).plot(140, 40, 40, 40)
+	.pause()
+)
 
 lines.stroke({
-  width: 2,
-  color: '#000'
+	width: 2,
+	color: '#000'
 })
 
 var all = draw.group().add(lines).add(circles)
 
-all.animate(duration).rotate(45).after(() => {lineAnims.forEach(a => a.play().after(() => {lineAnims2.forEach(a => a.play())}))})
+all.click(animate)
+
+function animate() {
+	all.animate(duration).rotate(rotation)
+	lineAnims.forEach(a => a.play())
+	circleAnims.forEach(a => a.play())
+}
