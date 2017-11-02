4a9a45f3df11e98b38244c1f0c8d6f7f80dbc8c6
Marco <marco.mojica@artcom.de>
2017-11-02 16:57:18 +0100

    Add slider for animation playback

--- svg-main.js
+++ svg-main.js
@@ -9,71 +9,81 @@ draw.circle().radius(4).center(90, 90)
 
 var lineAnims = []
 var circleAnims = []
-
-var rotation = 45;
+var rotations = [];
 
 
 circleAnims.push(
-	circles.circle().radius(4).center(90, 90)
-	.animate(duration).center(40, 40)
-	.pause()
+  circles.circle().radius(1).center(90, 90)
+  .animate(duration, '>').center(40, 40).radius(4)
+  .pause()
 )
 circleAnims.push(
-	circles.circle().radius(4).center(90, 90)
-	.animate(duration).center(140, 40)
-	.pause()
+  circles.circle().radius(1).center(90, 90)
+  .animate(duration, '>').center(140, 40).radius(4)
+  .pause()
 )
 circleAnims.push(
-	circles.circle().radius(4).center(90, 90)
-	.animate(duration).center(140, 140)
-	.pause()
+  circles.circle().radius(1).center(90, 90)
+  .animate(duration, '>').center(140, 140).radius(4)
+  .pause()
 )
 circleAnims.push(
-	circles.circle().radius(4).center(90, 90)
-	.animate(duration).center(40, 140)
-	.pause()
+  circles.circle().radius(1).center(90, 90)
+  .animate(duration, '>').center(40, 140).radius(4)
+  .pause()
 )
 
 lineAnims.push(
-	lines.line(40, 40, 40, 40)
-	.animate(duration / 3).plot(40, 40, 140, 40)
-	.animate(duration / 3).plot(140, 40, 140, 40)
-	.animate(duration / 3).plot(140, 140, 140, 40)
-	.pause()
+  lines.line(40, 40, 40, 40)
+  .animate(duration / 3).plot(40, 40, 140, 40)
+  .animate(duration / 3).plot(140, 40, 140, 40)
+  .animate(duration / 3).plot(140, 140, 140, 40)
+  .pause()
 )
 lineAnims.push(
-	lines.line(140, 40, 140, 40)
-	.animate(duration / 3).plot(140, 40, 140, 140)
-	.animate(duration / 3).plot(140, 140, 140, 140)
-	.animate(duration / 3).plot(40, 140, 140, 140)
-	.pause()
+  lines.line(140, 40, 140, 40)
+  .animate(duration / 3).plot(140, 40, 140, 140)
+  .animate(duration / 3).plot(140, 140, 140, 140)
+  .animate(duration / 3).plot(40, 140, 140, 140)
+  .pause()
 )
 lineAnims.push(
-	lines.line(140, 140, 140, 140)
-	.animate(duration / 3).plot(140, 140, 40, 140)
-	.animate(duration / 3).plot(40, 140, 40, 140)
-	.animate(duration / 3).plot(40, 40, 40, 140)
-	.pause()
+  lines.line(140, 140, 140, 140)
+  .animate(duration / 3).plot(140, 140, 40, 140)
+  .animate(duration / 3).plot(40, 140, 40, 140)
+  .animate(duration / 3).plot(40, 40, 40, 140)
+  .pause()
 )
 lineAnims.push(
-	lines.line(40, 140, 40, 140)
-	.animate(duration / 3).plot(40, 140, 40, 40)
-	.animate(duration / 3).plot(40, 40, 40, 40)
-	.animate(duration / 3).plot(140, 40, 40, 40)
-	.pause()
+  lines.line(40, 140, 40, 140)
+  .animate(duration / 3).plot(40, 140, 40, 40)
+  .animate(duration / 3).plot(40, 40, 40, 40)
+  .animate(duration / 3).plot(140, 40, 40, 40)
+  .pause()
 )
 
 lines.stroke({
-	width: 2,
-	color: '#000'
+  width: 2,
+  color: '#000'
 })
 
-var all = draw.group().add(lines).add(circles)
+rotations.push(lines.animate(duration).rotate(-(45 * 5)).pause())
+rotations.push(circles.animate(duration).rotate(45 * 3).pause())
 
-all.click(animate)
+/*
+lineAnims.forEach(a => a.play())
+circleAnims.forEach(a => a.play())
+rotations.forEach(a => a.play())
+*/
 
-function animate() {
-	all.animate(duration).rotate(rotation)
-	lineAnims.forEach(a => a.play())
-	circleAnims.forEach(a => a.play())
+function animate(at) {
+  lineAnims.forEach(a => a.at(at))
+  circleAnims.forEach(a => a.at(at))
+  rotations.forEach(a => a.at(at))
+}
+
+document.getElementById('slider').oninput = (event) => {
+  let val = event.target.value / 1001
+  console.log(val)
+  animate(val)
 }
