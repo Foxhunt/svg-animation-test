d4de1a7448af9cd469989aea0c1b192817577618
Marco <marco.mojica@artcom.de>
2018-01-08 12:33:29 +0100

    Use animation progress state from controlls

--- libs/Controlls.js
+++ libs/Controlls.js
@@ -1,98 +1,112 @@
 class Controlls {
 
-	constructor(svgDivID, duration) {
-		this.svgDiv = document.getElementById(svgDivID)
-		this.createControlls(svgDivID)
-		this.slider.max = duration
-		this.interval = null
-		this.duration = duration
-		this.animations = []
+  constructor(svgDivID, duration) {
+    this.svgDiv = document.getElementById(svgDivID)
+    this.createControlls(svgDivID)
+    this.slider.max = duration
+    this.interval = null
+    this.duration = duration
+    this.animations = []
 
-		this.startTime = 0;
-		this.lastTime = 0;
+    this.at = 0
 
-		this.init()
-	}
+    this.startTime = 0;
+    this.lastTime = 0;
 
-	init() {
-		this.slider.oninput = (e) => this.onSlide(e)
-		this.playPause.onclick = () => this.onClick()
-	}
+    this.init()
+  }
 
-	onSlide(event) {
-		let val = Number(this.slider.value) / (this.duration + 1)
-		this.animate(val)
-	}
+  init() {
+    this.slider.oninput = (e) => this.onSlide(e)
+    this.playPause.onclick = () => this.onClick()
+  }
 
-	onClick() {
-		if (this.slider.value == this.duration) {
-			this.slider.value = 0
-			this.startTime = Date.now()
-			this.lastTime = this.startTime
-			clearInterval(this.interval)
-			this.interval = setInterval(() => this.play(), 16)
-		} else if (this.interval) {
-			clearInterval(this.interval)
-			this.interval = null
-		} else {
-			this.startTime = Date.now()
-			this.lastTime = this.startTime
-			this.interval = setInterval(() => this.play(), 16)
-		}
-	}
+  onSlide(event) {
+    this.at = Number(this.slider.value) / (this.duration + 1)
+    this.animate(this.at)
+  }
 
-	animate(at) {
-		this.animations.forEach(an => an.forEach(a => a.at(at)))
-	}
+  onClick() {
+    if (this.slider.value == this.duration) {
+      this.slider.value = 0
+      this.startTime = Date.now()
+      this.lastTime = this.startTime
+      clearInterval(this.interval)
+      this.interval = setInterval(() => this.play(), 16)
+    } else if (this.interval) {
+      clearInterval(this.interval)
+      this.interval = null
+    } else {
+      this.startTime = Date.now()
+      this.lastTime = this.startTime
+      this.interval = setInterval(() => this.play(), 16)
+    }
+  }
 
-	play() {
-		let now = Date.now()
-		let pos = now - this.startTime
-		let delta = now - this.lastTime
-		this.lastTime = now
+  slideBy(value) {
+    let at = Number(this.slider.value) + (this.slider.max * value)
+    if (at >= this.slider.max) {
+      at = this.slider.max - 1
+    } else if (at < 0) {
+      at = 0
+    }
+    this.slider.value = at
+    this.onSlide()
+  }
 
-		if (Number(this.slider.value) < this.duration) {
-			this.slider.stepUp(delta)
-			this.animate(Number(this.slider.value) / (this.duration + 1))
-		} else if (this.loopBox.checked) {
-			this.slider.value = 0
-			this.startTime = Date.now()
-		} else {
-			clearInterval(this.interval)
-			this.interval = null
-		}
-	}
+  animate(at) {
+    this.animations.forEach(an => an.forEach(a => a.at(at)))
+  }
 
-	addAnimations(an) {
-		this.animations.push(an)
-	}
+  play() {
+    let now = Date.now()
+    let pos = now - this.startTime
+    let delta = now - this.lastTime
+    this.lastTime = now
 
-	createControlls(svgDivID) {
-		let playPause = document.createElement('input')
-		playPause.id = svgDivID + 'PlayPause'
-		playPause.type = 'button'
-		playPause.value = 'play'
+    if (Number(this.slider.value) < this.duration) {
+      this.slider.stepUp(delta)
+      this.at = Number(this.slider.value) / (this.duration + 1)
+      this.animate(this.at)
+    } else if (this.loopBox.checked) {
+      this.slider.value = 0
+      this.startTime = Date.now()
+    } else {
+      clearInterval(this.interval)
+      this.interval = null
+    }
+  }
 
-		let slider = document.createElement('input')
-		slider.id = svgDivID + 'Slider'
-		slider.type = 'range'
-		slider.value = '0'
-		
-		let loopBox = document.createElement('input')
-		loopBox.id = svgDivID + 'Loop'
-		loopBox.type = 'checkbox'
-		
-		let loopText = document.createTextNode("loop")
-		
-		
-		this.svgDiv.parentNode.insertBefore(playPause, this.svgDiv.nextSibling)
-			.parentNode.insertBefore(slider, playPause.nextSibling)
-			.parentNode.insertBefore(loopBox, slider.nextSibling)
-			.parentNode.insertBefore(loopText, loopBox.nextSibling)
-			
-		
-		this.playPause = playPause
-		this.slider = slider
-		this.loopBox = loopBox
-	}
+  addAnimations(an) {
+    this.animations.push(an)
+  }
+
+  createControlls(svgDivID) {
+    let playPause = document.createElement('input')
+    playPause.id = svgDivID + 'PlayPause'
+    playPause.type = 'button'
+    playPause.value = 'play'
+
+    let slider = document.createElement('input')
+    slider.id = svgDivID + 'Slider'
+    slider.type = 'range'
+    slider.value = '0'
+
+    let loopBox = document.createElement('input')
+    loopBox.id = svgDivID + 'Loop'
+    loopBox.type = 'checkbox'
+
+    let loopText = document.createTextNode("loop")
+
+
+    this.svgDiv.parentNode.insertBefore(playPause, this.svgDiv.nextSibling)
+      .parentNode.insertBefore(slider, playPause.nextSibling)
+      .parentNode.insertBefore(loopBox, slider.nextSibling)
+      .parentNode.insertBefore(loopText, loopBox.nextSibling)
+
+
+    this.playPause = playPause
+    this.slider = slider
+    this.loopBox = loopBox
+  }
 }
--- main.js
+++ main.js
@@ -1,23 +1,16 @@
 var ws = new WebSocket('ws://localhost:8080/')
 
-var at = 0
+ws.onmessage = function (event) {
+  let data = event.data.split(',')
 
-ws.onmessage = function(event) {
-    let data = event.data.split(',')
-    
-    console.log(data[0])
+  console.log(data[0], data[1])
 
-    if (data[0] === 'R') {
-      if (at >= 0 && at <= 1)
-        console.log(at += (Number(data[1]) / (360*10)))
-      else if (at > 0.9)
-        at = 0.99999999999
-      else if (at < 0)
-        at = 0
-      
-      if (at >= 0 && at < 1){
-        artsControlls.animate(at)
-        controlls.animate(at)
-      }
-    }
+  if (data[0] === 'R') {
+    let delta = (Number(data[1]) / (360 * 30))
+    artsControlls.slideBy(delta)
+    controlls.slideBy(delta)
+  } else if (data[0] === 'B' && data[1] == 1) {
+    artsControlls.onClick()
+    controlls.onClick()
   }
+}
